b65b4dd61b01c6cd64bb747a2d19de39be86819f
Codrin Ilie <125357154+CodyI20@users.noreply.github.com>
2023-11-30 13:51:44 +0100

    Changed the room spawning

--- VRingus/Assets/Scripts/ObstacleDespawner.cs
+++ VRingus/Assets/Scripts/ObstacleDespawner.cs
@@ -4,15 +4,13 @@ public class ObstacleDespawner : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.transform.parent == null)
-            return;
         if (other.CompareTag("Obstacle"))
         {
             Destroy(other.transform.parent.gameObject);
         }
         if (other.CompareTag("Room"))
         {
-            Destroy(other.transform.parent.gameObject);
+            Destroy(other.gameObject);
         }
     }
 }
